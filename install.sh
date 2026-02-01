023338a0b27f9e14e42d1e8cbb8d79768a241f90
leo <you@example.com>
2026-02-01 17:59:12 +0800

    x

--- install.sh
+++ install.sh
@@ -2,33 +2,48 @@
 
 ## --python=3.11 torch=2.8 有flash-attn=2.8
 ## --python=3.12 torch=2.9 有flash-attn=2.8
-uv venv --python 3.11 .venv
-TORCH_VERSION=2.8.0
+uv venv --python 3.12 .venv
+TORCH_VERSION=2.9.1
+mkdir -p .deps
 
 sed -i 's/VIRTUAL_ENV_PROMPT=.*/VIRTUAL_ENV_PROMPT="X2V"/g' .venv/bin/activate
 source .venv/bin/activate
 
 uv sync
+uv pip install torch torchaudio torchvision "torch~=$TORCH_VERSION"
 
+## 安装 flash_attn2
 uv pip install flash-attn --no-build-isolation "torch~=$TORCH_VERSION"
+#uv pip install flashinfer-python  "torch~=$TORCH_VERSION"
 
-mkdir .deps
+## 安装flash_attn3
+#git clone https://github.com/Dao-AILab/flash-attention.git .deps/flash-attention
+#cd .deps/flash-attention/hopper
+#CFLAGS="-O2" CXXFLAGS="-O2" python setup.py install
+#cd ../../../
 
 ## 安装 sageattention
 git clone https://github.com/thu-ml/SageAttention.git .deps/SageAttention
 ### 安装 sageattention2
 cd .deps/SageAttention
-CFLAGS="-O2" CXXFLAGS="-O2" python setup.py install
-cd ../../
+rm -rf build
+CFLAGS="-O2" CXXFLAGS="-O2" python setup.py install 
+#cd ../../
 ### 安装 sageattention3
-#cd sageattention3_blackwell
-#CFLAGS="-O2" CXXFLAGS="-O2" python setup.py install
-#cd ../../../
+cd sageattention3_blackwell
+CFLAGS="-O2" CXXFLAGS="-O2" python setup.py install
+cd ../../../
+
+## 安装 spas_sage_attn 
+git clone https://github.com/thu-ml/SpargeAttn.git .deps/SpargeAttn
+cd .deps/SpargeAttn
+CFLAGS="-O2" CXXFLAGS="-O2" python setup.py install
 
 
 ## 2. Install NVFP4 Kernel
 uv pip install scikit_build_core
 cd lightx2v_kernel
+rm -rf build
 git clone https://github.com/NVIDIA/cutlass.git
 ### 依赖 cutlass
 MAX_JOBS=$(nproc) CMAKE_BUILD_PARALLEL_LEVEL=$(nproc) \
@@ -37,12 +52,20 @@ MAX_JOBS=$(nproc) CMAKE_BUILD_PARALLEL_LEVEL=$(nproc) \
         -Ccmake.define.CUTLASS_PATH=./cutlass \
         --verbose --color=always --no-build-isolation
 uv pip install dist/*whl --force-reinstall --no-deps
+cd ../
 
 
-
-
+#git clone https://github.com/Lightricks/LTX-Video-Q8-Kernels.git .deps/LTX-Video-Q8-Kernels
+#cd .deps/LTX-Video-Q8-Kernels
+#CFLAGS="-O2" CXXFLAGS="-O2" python setup.py install
+uv pip install vllm "torch~=$TORCH_VERSION"
+uv pip install sgl-kernel "torch~=$TORCH_VERSION"
+#git clone https://github.com/vllm-project/vllm.git .deps/vllm
+#cd .deps/vllm
+#uv pip install -e . "torch~=$TORCH_VERSION"
 
 python << EOF
+
 import torch
 print("Torch version:", torch.__version__)
 print("CUDA available:", torch.cuda.is_available())
@@ -58,20 +81,35 @@ print("Torchaudio version:", torchaudio.__version__)
 import torchvision
 print("Torchvision version:", torchvision.__version__)
 
-import flash_attn
-from flash_attn import flash_attn_qkvpacked_func, flash_attn_func
-print("Flash Attention version:", flash_attn.__version__)
-print("Flash Attention flash_attn_func:", flash_attn_func)
+try:
+  import flash_attn
+  #from flash_attn import flash_attn_qkvpacked_func, flash_attn_func
+  print("Flash Attention 2 version:", flash_attn.__version__)
+except ImportError:
+   print("no install flash_attn2")
 
+try:
+  import flash_attn_interface
+  #from flash_attn_interface import flash_attn_varlen_func as flash_attn_varlen_func_v3
+  print("Flash Attention 3 version:", flash_attn_interface)
+except ImportError:
+   print("no install flash_attn3")
 
 try:
   from sageattention import sageattn
-  print("SageAttention version: sageattn2")
+  print("sageattn2 installed")
 except ImportError:
-  try:
-    from sageattn3 import sageattn3_blackwell
-    print("SageAttention sageattn3_blackwell:", sageattn3_blackwell)
-  except ImportError:
-     print("no install SageAttention ")
+  print("no install sageattn2 ")
+    
+try:
+  from sageattn3 import sageattn3_blackwell
+  print("sageattn3_blackwell installed")
+except ImportError:
+  print("no install sageattn3_blackwell ")
 
+try:
+  from q8_kernels.functional.linear import q8_linear
+  print(f"install q8_kernels")
+except ImportError:
+  print(f"no install q8_kernels")
 EOF
